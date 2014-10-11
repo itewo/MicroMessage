a3a4cf4f31aa454e4b2e7bb0c0a7f4eb7bf46970
itewo <itewo@qq.com>
2014-10-11 14:33:29 +0800

    方法独立

--- src/com/itewo/servlet/LeftServlet.java
+++ src/com/itewo/servlet/LeftServlet.java
@@ -1,10 +1,13 @@
 package com.itewo.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.itewo.service.ListService;
 
 /**
  * Servlet implementation class LeftServlet
@@ -32,11 +35,34 @@ public class LeftServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String type = request.getParameter("type");
-		if(type!=null&&"datalist".equals(type)){
-			//跳转到指定的页面
-			request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+		
+		if(type!=null&&"datalist".equals(type)){//数据列表请求
+			this.showDateList(request, response);
 		}
 		
 	}
+	
+	/**
+	 * 返回数据列表信息
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	protected void showDateList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//获取页面的值
+		String command =request.getParameter("command");
+		String description =request.getParameter("description");
+		//向页面返回值
+		request.setAttribute("command",command);
+		request.setAttribute("description",description);
+		//查询消息列表并传给页面
+		ListService  listservice = new ListService();
+		request.setAttribute("countnum", listservice.queryMessageList(command, description).size());
+		request.setAttribute("messageList", 	listservice.queryMessageList(command, description));
+		//跳转到指定的页面
+		request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+	}
+	
 
 }
